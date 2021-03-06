4d4bab6cd816b4767e5ea6a5bb0cf16f68dd6abf
Adam Hasselwander <adam.hasselwander@gmail.com>
2021-03-06 13:28:31 +0100

    Update Tests_Exact_Cover_No_Noise.m
    Added the possiblity to plot all noisemodels in a gid for easier comparision

--- tests/Tests_Exact_Cover_No_Noise.m
+++ tests/Tests_Exact_Cover_No_Noise.m
@@ -1,10 +1,39 @@
 close all
 
-plot_exact_cover_depo()
-plot_exact_cover_bitflip()
-plot_exact_cover_phaseflip()
-plot_exact_cover_ampdamp()
-plot_exact_cover_phasedamp()
+% plot_exact_cover_depo()
+% plot_exact_cover_bitflip()
+% plot_exact_cover_phaseflip()
+% plot_exact_cover_ampdamp()
+% plot_exact_cover_phasedamp()
+
+plot_exact_cover_all_noisemodels()
+
+function plot_exact_cover_all_noisemodels()
+    folders = ["depo","phasedamp", "ampdamp", "phaseflip", "bitflip"];
+    j = 0;
+    for folder=folders
+        j = j + 1;
+        subplot(2, 3, j)
+        files= dir(fullfile('data','exact_cover',folder,'*_f*.mat'));
+        %is = 1:length(files);
+        is = [1 10 18];
+        
+        fidelity = [];
+        for i=is
+            data = load(fullfile(files(i).folder, files(i).name));
+            fidelity = [fidelity data.fidelity];
+            b = bar(data.dist_keys, data.dist_values);
+            alpha 0.7
+            hold on
+            % Well, it looked beautiful in my mind!
+            x = xline(data.mean,'HandleVisibility','off');
+            x.Color = b.FaceColor;
+            x.LineWidth = 2;
+        end
+        title(folder)
+        legend(num2str(fidelity', 'Fidelity: %.2f'))
+    end
+end
 
 function plot_exact_cover_depo()
 	files= dir(fullfile('data','exact_cover','depo', 'depo_f*.mat'));
