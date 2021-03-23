da342c78722f52f9092b9f46d7030978eca7d3d2
Adam Hasselwander <adam.hasselwander@gmail.com>
2021-03-23 09:53:57 +0100

    Update Tests_Exact_Cover_No_Noise.m
    Bars besides each other instead of on top

--- tests/Tests_Exact_Cover_No_Noise.m
+++ tests/Tests_Exact_Cover_No_Noise.m
@@ -17,21 +17,34 @@ function plot_exact_cover_all_noisemodels()
         subplot(2, 3, j)
         files= dir(fullfile('data','exact_cover',folder,'*_f*.mat'));
         %is = 1:length(files);
-        is = [1 10 18];
+        is = [1 10 18 19 20];
         
         fidelity = [];
+        bar_data_x = [];
+        bar_data_y = [];
+        bar_mean = [];
         for i=is
             data = load(fullfile(files(i).folder, files(i).name));
             fidelity = [fidelity data.fidelity];
-            b = bar(data.dist_keys, data.dist_values);
+            bar_data_x = [bar_data_x; [data.dist_keys]];
+            bar_data_y = [bar_data_y [data.dist_values]'];
+            bar_mean = [bar_mean, data.mean];
+        end
+        
+        b = bar(bar_data_x(1, :), bar_data_y);
+        colors =  vfunc(b.FaceColor);
+        
+        for i=1:length(is)
             alpha 0.7
             hold on
             % Well, it looked beautiful in my mind!
-            x = xline(data.mean,'HandleVisibility','off');
-            x.Color = b.FaceColor;
+            x = xline(bar_mean(i),'HandleVisibility','off');
+            x.Color = colors(i, :);
             x.LineWidth = 2;
         end
         title(folder)
+        xlabel('Cost, C(z)')
+        ylabel('Probability, P^{(\gamma, \beta)})')
         legend(num2str(fidelity', 'Fidelity: %.2f'))
     end
 end
@@ -155,4 +168,13 @@ function plot_exact_cover_phasedamp()
     end
 	legend(string(1:length(files)))
 
+    
+end
+
+function not_a_cell=vfunc(varargin)
+    not_a_cell = [];
+    for v=varargin
+        c = v{1};
+        not_a_cell = [not_a_cell; c];
+    end
 end
