5e747a35f0178ffa9fba7d4859ac7375df1e6033
Filip9707 <dahlenf@student.chalmers.se>
2021-03-23 10:25:59 +0100

    Update Matlab code

--- tests/Tests_Exact_Cover_No_Noise.m
+++ tests/Tests_Exact_Cover_No_Noise.m
@@ -7,7 +7,7 @@ close all
 % plot_exact_cover_phasedamp()
 
 %plot_exact_cover_all_noisemodels()
-plot_equal_size_partition_all_noisemodels()
+%plot_equal_size_partition_all_noisemodels()
 
 function plot_exact_cover_all_noisemodels()
     folders = ["depo","phasedamp", "ampdamp", "phaseflip", "bitflip"];
@@ -39,11 +39,17 @@ end
 
 function plot_equal_size_partition_all_noisemodels()
     folders = ["depo","phasedamp", "ampdamp", "phaseflip", "bitflip"];
-    j = 0;
-    for folder=folders
+    subfolders = ["[ 3  6  7 10]","[ 4  6  8 10]","[ 1  1  3  5  8 10]","[ 3  4  8  8  9 10]","[ 2  6  6  8  8  8  8 10]","[ 1  1  3  6  7  9  9 10 10 10]","[ 1  3  5  6  6  8  9  9  9 10]","[ 2  2  3  3  4  5  5  6  7  8  9 10]","[ 2  3  4  4  6  8  9 10]","[1 1 1 2 3 3 4 5 6 6 9 9]"];
+    
+    for folder1=subfolders
+        
+        figure('Name', folder1)
+        j = 0;
+        for folder=folders
+        
         j = j + 1;
         subplot(2, 3, j)
-        files= dir(fullfile('data','equal_size_partition',folder,'*_f*.mat'));
+        files= dir(fullfile('data','equal_size_partition',folder, folder1,'*_f*.mat'));
         %is = 1:length(files);
         is = [1 10 18];
         
@@ -61,6 +67,8 @@ function plot_equal_size_partition_all_noisemodels()
         end
         title(folder)
         legend(num2str(fidelity', 'Fidelity: %.2f'))
+        end
+        
     end
 end
 
