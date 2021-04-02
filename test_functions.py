cb47b295c338e9303665ecc7f093c02010836a11
davitpetrosyan99 <79146950+davitpetrosyan99@users.noreply.github.com>
2021-04-02 16:52:11 +0200

    Test function to test Qiskit optimization techniques. Work in progress

--- test_functions.py
+++ test_functions.py
@@ -1,9 +1,19 @@
 from qiskit import QuantumCircuit
 import numpy as np
 import matplotlib
-from swap_network import *
-from compare_routing_techniques import *
-from linear_swap import *
+from swap_network import swap_network, get_qubit_grid
+from linear_swap import linear_swap_method
+from equal_size_partition.get_circuit import get_circuit
+from equal_size_partition.gen_equal_size_partition_data import decode_file
+from equal_size_partition.decode_state import decode_state
+from equal_size_partition.get_ising_model import get_ising_model
+from qiskit.transpiler import PassManager, passes, CouplingMap, Layout
+from optimize_circuit import optimize_mapping
+from native_gate_set import translate_circuit
+from qiskit import IBMQ
+from qiskit import Aer
+
+
 
 def decomposing_circuit():
     circuit = QuantumCircuit(7)
@@ -131,4 +141,44 @@ def test_linear_routing():
     circuit = linear_swap_method(J, gamma, beta)
     circuit.draw(output='mpl', filename='SWAP_ladder')
 #test_swap_network()
-test_linear_routing()
+#test_linear_routing()
+
+def test_optimization():
+
+    decoded_file = decode_file('example_data_q4_q20')
+    IBMQ.save_account('APIKEY', overwrite=True)
+    IBMQ.load_account()
+    provider = IBMQ.get_provider(hub='ibm-q')
+    backend = provider.get_backend('ibmq_16_melbourne')
+    for i, (arr, sol) in enumerate(decoded_file):
+        
+        if len(arr) != 4: continue
+
+        if len(arr) not in [2**k for k in range(10)]:
+            print('PASS')
+            continue # only 2**k numbers of qubits allowed
+            # 4, 8, 16 out of q4_to_q20
+
+        S = np.array(arr)
+        gamma = np.random.rand() * np.pi
+        beta = np.random.rand() * np.pi
+
+        J, h, bounds = get_ising_model(S)
+        qaoa_circuit = get_circuit(gamma, beta, J, h)
+        print(qaoa_circuit)
+        qubit_grid = get_qubit_grid(len(arr))
+
+        coupling = CouplingMap()
+        rows, cols = qubit_grid.shape
+
+        grid_coupling_swap_network = coupling.from_grid(rows, cols)
+        optimized_circ = optimize_mapping(qaoa_circuit, backend, 3)
+
+        swap_network_circuit = swap_network(qaoa_circuit, qubit_grid) 
+        print(swap_network_circuit.depth())
+        swap_network_circuit_optimized = swap_network(optimized_circ, qubit_grid)       
+        optimized_circ = optimize_mapping(swap_network_circuit_optimized, backend, 3)
+        print(optimized_circ.depth())
+
+test_optimization()
+
