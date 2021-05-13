b4bfc0f431b1e17e7f52d86a932cb11cb285a5d4
ZeX2 <zeidanzeze@gmail.com>
2021-05-14 01:24:36 +0200

    New simulation!

--- problem_list_landscape.py
+++ problem_list_landscape.py
@@ -17,7 +17,7 @@ def get_problem_list():
                     #{'W_max': 1, 'W': [1, 1, 1], 'A': 12, 'B': 4, 'C': 24, 'p': 1, 'noise': False},
                     #{'W_max': 1, 'W': [1, 1, 1], 'A': 12, 'B': 4, 'C': 48, 'p': 1, 'noise': True},
                     #{'W_max': 1, 'W': [1, 1, 1], 'A': 12, 'B': 4, 'C': 48, 'p': 1, 'noise': False}
-                    {'W_max': 2, 'W': [1, 1], 'A': 32, 'B': 4, 'C': 576, 'p': 1, 'noise': False},
-                    {'W_max': 2, 'W': [1, 1], 'A': 32, 'B': 4, 'C': 64, 'p': 1, 'noise': False},
+                    {'W_max': 2, 'W': [1, 1], 'A': 36, 'B': 4, 'C': 684, 'p': 1, 'noise': False},
+                    {'W_max': 2, 'W': [1, 1], 'A': 36, 'B': 4, 'C': 72, 'p': 1, 'noise': False},
                     {'W_max': 2, 'W': [1, 1], 'p': 1, 'noise': True, 'routing': 'network'}]
     return problem_list
